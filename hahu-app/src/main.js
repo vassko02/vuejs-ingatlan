7a86322e1251c81d10946262284dc488d0bf146b
Vass Kornél <vass.kornel@gmail.com>
2022-03-29 11:34:29 +0200

    komponent

--- hahu-app/src/main.js
+++ hahu-app/src/main.js
@@ -2,7 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 const app =createApp(App);
 
+import { createRouter,createWebHistory } from 'vue-router';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap'
+import OpenPage from "./components/OpenPage.vue";
+import IngatlanKinalat from "./components/IngatlanKinalat.vue";
 
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes: [
+        {path:'/',component:OpenPage},  
+        {path:'/offers',component:IngatlanKinalat}
+    ]
+});
+app.use(router);
 app.mount('#app')
