fb5d9a067d6c706b704ca6ddf72436145c833de7
Vass Kornél <vass.kornel@gmail.com>
2022-03-29 11:04:11 +0200

    bajnok

--- hahu-app/src/main.js
+++ hahu-app/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+const app =createApp(App);
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap'
 
-createApp(App).mount('#app')
+app.mount('#app')
